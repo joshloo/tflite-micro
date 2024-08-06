f5e7853cb421a19dd0a09565de58c14193d2bf40
Josh Loo <Tunglun.Loo@latticesemi.com>
2024-08-06 15:22:31 +0800

    Add PDM required operations

--- tensorflow/lite/micro/examples/person_detection/person_detection_test.cc
+++ tensorflow/lite/micro/examples/person_detection/person_detection_test.cc
@@ -24,6 +24,38 @@ limitations under the License.
 #include "tensorflow/lite/micro/testing/micro_test.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
+// Got already on top
+//#include "tensorflow/lite/micro/examples/person_detection/person_detect_model_data.h"
+//#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+//#include "tensorflow/lite/micro/micro_interpreter.h"
+
+// to add
+#include "tensorflow/lite/micro/tflite_bridge/micro_error_reporter.h"
+#include "tensorflow/lite/micro/kernels/micro_ops.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/micro/system_setup.h"
+
+// not sure if can go through
+//#include "tensorflow/lite/version.h"
+
+// Globals, used for compatibility with Arduino-style sketches.
+namespace {
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+
+// An area of memory to use for input, output, and intermediate arrays.
+#if defined(XTENSA) && defined(VISION_P6)
+constexpr int tensor_arena_size = 352 * 1024;
+#else
+constexpr int tensor_arena_size = 136 * 1024;
+#endif  // defined(XTENSA) && defined(VISION_P6)
+//constexpr int tensor_arena_size = 93 * 1024;
+alignas(16) static uint8_t tensor_arena[tensor_arena_size] = {0};
+}  // namespace
+
+/*
 // Create an area of memory to use for input, output, and intermediate arrays.
 #if defined(XTENSA) && defined(VISION_P6)
 constexpr int tensor_arena_size = 352 * 1024;
@@ -31,40 +63,66 @@ constexpr int tensor_arena_size = 352 * 1024;
 constexpr int tensor_arena_size = 136 * 1024;
 #endif  // defined(XTENSA) && defined(VISION_P6)
 uint8_t tensor_arena[tensor_arena_size];
+*/
 
 TF_LITE_MICRO_TESTS_BEGIN
 
 TF_LITE_MICRO_TEST(TestInvoke) {
+
+  // Follow PDM flow
+  // Step 1: Get error reporter
+  static tflite::MicroErrorReporter micro_error_reporter;
+  error_reporter = &micro_error_reporter;
+
+
+  // Step 2: Get model
   // Map the model into a usable data structure. This doesn't involve any
   // copying or parsing, it's a very lightweight operation.
-  const tflite::Model* model = ::tflite::GetModel(g_person_detect_model_data);
+  model = ::tflite::GetModel(g_person_detect_model_data);
   if (model->version() != TFLITE_SCHEMA_VERSION) {
-    MicroPrintf(
+    TF_LITE_REPORT_ERROR(error_reporter,
         "Model provided is schema version %d not equal "
         "to supported version %d.\n",
         model->version(), TFLITE_SCHEMA_VERSION);
   }
 
+  // Step 3: Get OpResolver to run the topology
   // Pull in only the operation implementations we need.
   // This relies on a complete list of all the ops needed by this graph.
   // An easier approach is to just use the AllOpsResolver, but this will
   // incur some penalty in code space for op implementations that are not
   // needed by this graph.
-  tflite::MicroMutableOpResolver<5> micro_op_resolver;
+  tflite::MicroMutableOpResolver<8> micro_op_resolver;
+
+  //micro_op_resolver.AddConv2D();
+  micro_op_resolver.AddMaxPool2D();
+  micro_op_resolver.AddQuantize();
+  micro_op_resolver.AddReshape();
+  micro_op_resolver.AddFullyConnected();
+
+  // Keeping below to make compilation happy
   micro_op_resolver.AddAveragePool2D(tflite::Register_AVERAGE_POOL_2D_INT8());
   micro_op_resolver.AddConv2D(tflite::Register_CONV_2D_INT8());
   micro_op_resolver.AddDepthwiseConv2D(
       tflite::Register_DEPTHWISE_CONV_2D_INT8());
-  micro_op_resolver.AddReshape();
+  //micro_op_resolver.AddReshape();
   micro_op_resolver.AddSoftmax(tflite::Register_SOFTMAX_INT8());
 
+  // Step 4: Add interpreter
   // Build an interpreter to run the model with.
-  tflite::MicroInterpreter interpreter(model, micro_op_resolver, tensor_arena,
+  tflite::MicroInterpreter static_interpreter(model, micro_op_resolver, tensor_arena,
                                        tensor_arena_size);
-  interpreter.AllocateTensors();
+  interpreter = &static_interpreter;
+  //interpreter.AllocateTensors();
+  // Add code to check allocate status
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+    return -1;
+  }
 
   // Get information about the memory area to use for the model's input.
-  TfLiteTensor* input = interpreter.input(0);
+  input = interpreter->input(0);
 
   // Make sure the input has the properties we expect.
   TF_LITE_MICRO_EXPECT(input != nullptr);
@@ -79,16 +137,18 @@ TF_LITE_MICRO_TEST(TestInvoke) {
   TFLITE_DCHECK_EQ(input->bytes, static_cast<size_t>(g_person_image_data_size));
   memcpy(input->data.int8, g_person_image_data, input->bytes);
 
+  // Step 5: Invoke
   // Run the model on this input and make sure it succeeds.
-  TfLiteStatus invoke_status = interpreter.Invoke();
+  TfLiteStatus invoke_status = interpreter->Invoke();
   if (invoke_status != kTfLiteOk) {
     MicroPrintf("Invoke failed\n");
   }
   TF_LITE_MICRO_EXPECT_EQ(kTfLiteOk, invoke_status);
 
+  // Step 6: Get output
   // Get the output from the model, and make sure it's the expected size and
   // type.
-  TfLiteTensor* output = interpreter.output(0);
+  TfLiteTensor* output = interpreter->output(0);
   TF_LITE_MICRO_EXPECT_EQ(2, output->dims->size);
   TF_LITE_MICRO_EXPECT_EQ(1, output->dims->data[0]);
   TF_LITE_MICRO_EXPECT_EQ(kCategoryCount, output->dims->data[1]);
@@ -104,7 +164,7 @@ TF_LITE_MICRO_TEST(TestInvoke) {
   memcpy(input->data.int8, g_no_person_image_data, input->bytes);
 
   // Run the model on this "No Person" input.
-  invoke_status = interpreter.Invoke();
+  invoke_status = interpreter->Invoke();
   if (invoke_status != kTfLiteOk) {
     MicroPrintf("Invoke failed\n");
   }
@@ -112,7 +172,7 @@ TF_LITE_MICRO_TEST(TestInvoke) {
 
   // Get the output from the model, and make sure it's the expected size and
   // type.
-  output = interpreter.output(0);
+  output = interpreter->output(0);
   TF_LITE_MICRO_EXPECT_EQ(2, output->dims->size);
   TF_LITE_MICRO_EXPECT_EQ(1, output->dims->data[0]);
   TF_LITE_MICRO_EXPECT_EQ(kCategoryCount, output->dims->data[1]);
